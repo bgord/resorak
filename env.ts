a7d55abd3453bdef5858125ef910465f52f96f81
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-24 18:52:20 +0200

    step: add twitter bearer token to env

--- env.ts
+++ env.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 import { Schema, EnvironmentValidator } from "@bgord/node";
 
+import { TwitterBearerToken } from "./value-objects/twitter-bearer-token";
+
 const EnvironmentSchema = z.object({
   PORT: Schema.Port,
+  TWITTER_BEARER_TOKEN: TwitterBearerToken,
 });
 type EnvironmentSchemaType = z.infer<typeof EnvironmentSchema>;
 
