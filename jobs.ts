20c9fc6fbb01b4344aba72f593e61db4e12db450
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-24 22:01:58 +0200

    step: fetch tweets for each feed

--- jobs.ts
+++ jobs.ts
@@ -1,10 +1,22 @@
 import { Reporter } from "@bgord/node";
 import { ToadScheduler, SimpleIntervalJob, AsyncTask } from "toad-scheduler";
 
+import { TwitterService } from "./services/twitter";
+import { TwitterRss } from "./aggregates/twitter-rss";
+
 export const Scheduler = new ToadScheduler();
 
 const task = new AsyncTask("twitter rss feed creator", async () => {
   Reporter.info("twitter rss feed creator");
+
+  const twitterRss = await new TwitterRss().build();
+
+  const twitterRssFeeds = twitterRss.getFeeds();
+
+  for (const feed of twitterRssFeeds) {
+    const tweets = await TwitterService.getTweets(feed.twitterUserName);
+    console.log(tweets);
+  }
 });
 
 const job = new SimpleIntervalJob({ minutes: 1, runImmediately: true }, task);
