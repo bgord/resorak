dce413fc16c23f151a6dbc6ab6663b84517b01f7
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-24 17:59:31 +0200

    feature: setup twitter rss feeds aggregate

---
+++ aggregates/twitter-rss.ts
@@ -0,0 +1,35 @@
+import { EventRepository } from "../repositories/event-repository";
+
+import { TwitterHandleType } from "../value-objects/twitter-handle";
+import {
+  CREATED_RSS_EVENT,
+  CreatedRssEvent,
+} from "../value-objects/created-rss-event";
+
+export class TwitterRssFeeds {
+  private feeds: TwitterHandleType[] = [];
+
+  async build() {
+    const createdRssEvents = await new EventRepository().find(
+      CREATED_RSS_EVENT
+    );
+
+    const feeds: TwitterHandleType[] = [];
+
+    for (const event of createdRssEvents) {
+      const { payload } = CreatedRssEvent.pick({ payload: true }).parse({
+        payload: event.payload,
+      });
+
+      feeds.push(payload.twitterHandle);
+    }
+
+    this.feeds = feeds;
+
+    return this;
+  }
+
+  getAll(): TwitterRssFeeds["feeds"] {
+    return this.feeds;
+  }
+}
