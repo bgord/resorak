f5ccb01e3ae2ff2917cac7347a7092f371bcb25a
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-24 19:44:49 +0200

    fix: inverted condition in twitter handle exists policy

--- policies/twitter-handle-exists.ts
+++ policies/twitter-handle-exists.ts
@@ -2,6 +2,6 @@ import { TwitterUserType } from "../value-objects/twitter-user";
 
 export class TwitterHandleExistsPolicy {
   static async fails(user: TwitterUserType | null): Promise<boolean> {
-    return user !== null;
+    return user === null;
   }
 }
