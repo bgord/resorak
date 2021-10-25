3c00dd4701a55a20557fe7a8c0dca60f9f577959
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-25 22:31:23 +0200

    step: emit REGENERATED_RSS_EVENT every 1 minute for each twitter rss feed

--- jobs.ts
+++ jobs.ts
@@ -1,7 +1,14 @@
 import { Reporter } from "@bgord/node";
 import { ToadScheduler, SimpleIntervalJob, AsyncTask } from "toad-scheduler";
 
+import { emittery } from "./events";
+
 import { TwitterRss } from "./aggregates/twitter-rss";
+
+import {
+  RegeneratedRssEvent,
+  REGENERATED_RSS_EVENT,
+} from "./value-objects/regenerated-rss-event";
 
 export const Scheduler = new ToadScheduler();
 
@@ -9,12 +16,15 @@ const task = new AsyncTask("twitter rss feed creator", async () => {
   Reporter.info("twitter rss feed creator");
 
   const twitterRss = await new TwitterRss().build();
-
   const twitterRssFeeds = twitterRss.getFeeds();
 
-  for (const feed of twitterRssFeeds) {
-    await twitterRss.generateFeed(feed);
-  }
+  const regeneratedRssEvent = RegeneratedRssEvent.parse({
+    name: REGENERATED_RSS_EVENT,
+    version: 1,
+    payload: twitterRssFeeds,
+  });
+
+  emittery.emit(REGENERATED_RSS_EVENT, regeneratedRssEvent);
 });
 
 const job = new SimpleIntervalJob({ minutes: 1, runImmediately: true }, task);
