2d3a5387b479c5117f8125eddb4c12b5c13625bc
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-25 23:24:51 +0200

    fix: add .rss extension to the saved file

--- services/twitter-rss-location-generator.ts
+++ services/twitter-rss-location-generator.ts
@@ -4,7 +4,7 @@ import { TwitterRssFeedType } from "../value-objects/twitter-rss-feed";
 
 export class TwitterRssLocationGenerator {
   static generate(feed: TwitterRssFeedType) {
-    const filename = feed.twitterUserId;
+    const filename = `${feed.twitterUserId}.rss`;
 
     return {
       filename,
