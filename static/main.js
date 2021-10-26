3c6ad62f8b65f2aac3774eb3db7f0ad54137959b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-26 12:43:29 +0200

    feature: add copy to clipboard button

--- static/main.js
+++ static/main.js
@@ -1,1 +1,25 @@
-console.log("hello from main.js");
+const copyButtons = [...document.querySelectorAll("button.copy")];
+
+for (const button of copyButtons) {
+  const link = button.dataset.link;
+
+  button.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(link);
+
+      button.disabled = true;
+      button.innerHTML = "Copied!";
+
+      await sleep(2500);
+
+      button.disabled = false;
+      button.innerHTML = "Copy";
+    } catch (error) {
+      console.error("Copy to clipboard not supported.");
+    }
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
