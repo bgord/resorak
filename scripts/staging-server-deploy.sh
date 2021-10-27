58ba7b45fcf2cd798d65d965cd89efb45ef86aad
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-27 21:27:11 +0200

    step: restart resorak service in the deployment script

--- scripts/staging-server-deploy.sh
+++ scripts/staging-server-deploy.sh
@@ -8,4 +8,7 @@ info "Environment: staging"
 rsync -azP --delete build/ staging:/var/www/resorak
 info "Synced source files"
 
+ssh staging "sudo systemctl restart resorak.service"
+info "Restarted resorak.service!"
+
 success "Deployed!"
