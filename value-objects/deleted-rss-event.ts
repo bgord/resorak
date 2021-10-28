37f73e46685894a47e1026b3826480bb92ee4c85
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-28 22:51:41 +0200

    step: add deleted rss event

---
+++ value-objects/deleted-rss-event.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { EventDraft } from "@bgord/node";
+
+import { TwitterId } from "../value-objects/twitter-id";
+
+export const DELETED_RSS_EVENT = "DELETED_RSS";
+
+export const DeletedRssEvent = EventDraft.merge(
+  z.object({
+    name: z.literal(DELETED_RSS_EVENT),
+    version: z.literal(1),
+    payload: z.object({
+      twitterId: TwitterId,
+    }),
+  })
+);
+
+export type DeletedRssEventType = z.infer<typeof DeletedRssEvent>;
