d14b13a6330839d3d232dd00eb2e05a1b12b2c18
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-10-29 18:53:10 +0200

    step: add SENTRY_DSN env variable

--- env.ts
+++ env.ts
@@ -8,6 +8,7 @@ const EnvironmentSchema = z.object({
   BASE_URL: Schema.UrlWithoutTrailingSlash,
   API_KEY: Schema.ApiKey,
   COOKIE_SECRET: Schema.CookieSecret,
+  SENTRY_DSN: Schema.SentryDsn,
 });
 type EnvironmentSchemaType = z.infer<typeof EnvironmentSchema>;
 
