242311fcf382302bc86083816aaef8112196553d
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-11-10 19:52:58 +0100

    step: add TwitterRssFeedStatusTransition policy

--- policies/index.ts
+++ policies/index.ts
@@ -2,3 +2,4 @@ export * from "./twitter-user-exists";
 export * from "./twitter-rss-feed-should-exist";
 export * from "./twitter-rss-feed-should-not-exist";
 export * from "./twitter-rss-feed-filter-transition";
+export * from "./twitter-rss-feed-status-transition";
---
+++ policies/twitter-rss-feed-status-transition.ts
@@ -0,0 +1,29 @@
+import * as VO from "../value-objects";
+
+export class TwitterRssFeedStatusTransition {
+  static fails(
+    from: VO.TwitterRssFeedStatusType,
+    to: VO.TwitterRssFeedStatusType
+  ): boolean {
+    const transitions: Record<
+      VO.TwitterRssFeedStatusType,
+      VO.TwitterRssFeedStatusType[]
+    > = {
+      [VO.TwitterRssFeedStatusEnum.active]: [
+        VO.TwitterRssFeedStatusEnum.suspended,
+      ],
+      [VO.TwitterRssFeedStatusEnum.suspended]: [
+        VO.TwitterRssFeedStatusEnum.active,
+      ],
+    };
+
+    return !transitions[from].includes(to);
+  }
+}
+
+export class TwitterRssFeedStatusTransitionError extends Error {
+  constructor() {
+    super();
+    Object.setPrototypeOf(this, TwitterRssFeedStatusTransitionError.prototype);
+  }
+}
