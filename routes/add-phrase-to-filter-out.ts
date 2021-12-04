7178192dadfdabd74a43621a7e578be0ccad3625
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-04 23:37:30 +0100

    step: save phrase to filter out

--- routes/add-phrase-to-filter-out.ts
+++ routes/add-phrase-to-filter-out.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import * as VO from "../value-objects";
+import { PhrasesToFilterOutRepository } from "../repositories/phrases-to-filter-out-repository";
 
 export async function AddPhraseToFilterOut(
   request: express.Request,
@@ -10,6 +11,8 @@ export async function AddPhraseToFilterOut(
   const phraseToFilterOut = VO.PhraseToFilterOut.parse(
     request.body.phraseToFilterOut
   );
+
+  await PhrasesToFilterOutRepository.save(phraseToFilterOut);
 
   return response.redirect("/");
 }
