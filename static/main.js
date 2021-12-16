8d73b838e7139a2ecd3ab86fd0f520284bfda048
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-16 23:00:38 +0100

    step: remove js code for selects

--- static/main.js
+++ static/main.js
@@ -49,37 +49,3 @@ for (const timestamp of timestamps) {
     timestamp.insertAdjacentHTML("beforeend", "N/A");
   }
 }
-
-const selects = [...document.querySelectorAll("select.action")];
-for (const select of selects) {
-  const { id } = select.dataset;
-
-  updateFormValue(id, select.value);
-
-  select.addEventListener("change", (event) =>
-    updateFormValue(id, event.target.value)
-  );
-
-  function updateFormValue(id, value) {
-    const form = document.querySelector(`form[data-id="${id}"]`);
-
-    if (value === "delete") {
-      form.action = `/delete-rss/${id}?_method=DELETE`;
-    }
-    if (value === "regenerate") {
-      form.action = `/regenerate-rss/${id}`;
-    }
-    if (value === "include-reply-tweets") {
-      form.action = `/include-reply-tweets-in-rss/${id}`;
-    }
-    if (value === "skip-reply-tweets") {
-      form.action = `/skip-reply-tweets-in-rss/${id}`;
-    }
-    if (value === "suspend") {
-      form.action = `/suspend-rss/${id}`;
-    }
-    if (value === "activate") {
-      form.action = `/activate-rss/${id}`;
-    }
-  }
-}
