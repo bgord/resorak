983ee1e6b84e6e09183f88b155eb016418b130d1
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-22 23:46:03 +0100

    feature: display username in header on dashboard

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -33,6 +33,7 @@ export async function Dashboard(
       : null,
 
     phrasesToFilterOut: await PhrasesToFilterOutRepository.find(),
+    username: request.user,
 
     error: messages[0],
     ...CsrfShield.extract(request),
